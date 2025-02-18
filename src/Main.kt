a8cfbbfd3b5230f22b10dced6220f2fbb7b5a453
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-02-18 20:12:42 +0300

    Done task

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,29 @@
 fun main() {
-    println("Hello World!")
+
+    var admin = UserRole.ADMIN
+    var editor = UserRole.EDITOR
+    var viewer = UserRole.VIEWER
+
+    println(checkAccess(admin))
+    println(checkAccess(editor))
+    println(checkAccess(viewer))
+
+    for (user in UserRole.entries){
+        println("Role: $user - Access level: ${user.accessLevel}")
+    }
+}
+
+
+
+enum class UserRole(val accessLevel: Int) {
+    ADMIN(100), EDITOR(20), VIEWER(10)
+}
+
+
+fun checkAccess(user: UserRole):String{
+    return when (user){
+         UserRole.ADMIN -> "You are an admin, you have full access"
+          UserRole.EDITOR -> "You are an editor, just do what you need and don't ruin anything!"
+        UserRole.VIEWER -> "You are just a viewer, you can look but you can't touch"
+    }
 }
